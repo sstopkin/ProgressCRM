d97803092c1028c992e6a12747c206f98b8cfc33
stopkin.sergey@gmail.com <sergeystopkin>
2013-09-12 04:47:05 +0000

    help desk API

---
+++ ProgressCRM/src/java/org/progress/crm/api/HelpDeskApi.java
@@ -0,0 +1,53 @@
+package org.progress.crm.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.sql.SQLException;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.hibernate.Session;
+import org.progress.crm.controllers.HelpDeskController;
+import org.progress.crm.exceptions.CustomException;
+import org.progress.crm.util.Command;
+import org.progress.crm.util.TransactionService;
+
+@Stateless
+@Path("helpdesk")
+public class HelpDeskApi {
+
+    @EJB
+    HelpDeskController helpDeskController;
+
+    @GET
+    @Path("getallrequest")
+    public Response getAllHelpDeskRequest() throws CustomException {
+        return TransactionService.runInScope(new Command<Response>() {
+            @Override
+            public Response execute(Session session) throws CustomException, SQLException {
+                Gson allHelpDeskRequest = new GsonBuilder().create();
+                String result = allHelpDeskRequest.toJson(helpDeskController.getAllHelpDeskRequest(session));
+                return ApiHelper.getResponse(result);
+            }
+        });
+    }
+
+    @POST
+    @Path("addrequest")
+    public Response addCustomer(@CookieParam("token") final String token,
+            @FormParam("request") final String request,
+            @FormParam("description") final String description) throws SQLException, CustomException {
+        return TransactionService.runInScope(new Command<Response>() {
+            @Override
+            public Response execute(Session session) throws CustomException, SQLException {
+                boolean result = helpDeskController.addHelpDeskRequest(session, token, request, description);
+                return ApiHelper.getResponse(result);
+            }
+        });
+    }
+}
