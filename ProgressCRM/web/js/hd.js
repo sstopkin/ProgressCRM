87c23b154c18af8a39f52123b4a5e7e15e19d307
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2013-09-18 18:21:41 +0000

    helpdesk impl

--- ProgressCRM/web/js/hd.js
+++ ProgressCRM/web/js/hd.js
@@ -2,94 +2,35 @@ function getHelpDeskPage() {
     $("#addApartaments").css("display", "none");
     $.get("hd.html", function(data) {
         $("#mainContainer").html(data);
+        $.ajax({
+            type: "GET",
+            url: "api/helpdesk/getallrequest",
+            success: function(data) {
+                $("#errorBlock").css("display", "none");
+                var array = JSON.parse(data);
+                var content = "";
+//
+//                console.log(array.apartamentsPhotosList);
+//
+//                console.log(array.apartaments.IsApproved);
+//                console.log(array.apartaments.deleted);
+//
+//
+//                content += "<p>";
+//                content += "ID = " + array.apartaments.id;
+//                content += "</p>";
+
+
+                $("#mainHelpDeskContainer").html(content);
+            },
+            error: function(data) {
+                showDanger(data.responseText);
+                return false;
+            }
+        });
     });
 }
 
-$("#addHelpDeskRequestModalBtn").click(function() {
-    alert("asd");
-//    $("#regErr").css("display", "none");
-//    if ($("#emailDiv").hasClass("has-error")) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Неверный Email");
-//        return false;
-//    }
-//
-//    if ($("#nameDiv").hasClass("has-error")) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Неверно задано имя");
-//        return false;
-//    }
-//
-//    if ($("#lnameDiv").hasClass("has-error")) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Неверно задана фамилия");
-//        return false;
-//    }
-//
-//    if ($("#passwordDiv").hasClass("has-error")) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Пароль должен содержать от 6 символов и\\или цифр");
-//        return false;
-//    }
-//
-//    if (($("#email").val() == "") || ($("#name").val() == "")
-//            || ($("#lname").val() == "") || ($("#password").val() == "")
-//            || ($("#password2").val() == "")) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Не все обязательные поля заполнены");
-//        return false;
-//    }
-//    if (($("#email").val().length > 50)
-//            || ($("#name").val().length > 50)
-//            || ($("#lname").val().length > 50)
-//            || ($("#password").val().length > 50)) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Превышена максимальная длина поля");
-//        return false;
-//    }
-//    if ($("#password").val() != $("#password2").val()) {
-//        $("#regErr").css("display", "inline");
-//        $("#regErr").html("Пароли не совпадают");
-//        return false;
-//    }
-//    $.ajax({
-//        type: "POST",
-//        url: "api/reg",
-//        data: ({
-//            email: $("#email").val(),
-//            name: $("#name").val(),
-//            lname: $("#lname").val(),
-//            pass: $("#password").val()
-//        }),
-//        success: function(data) {
-//            $("#registerModal").modal("hide");
-//            $("#regNames").html($("#name").val() + " " + $("#lname").val());
-//            $("#regEmails").html($("#email").val());
-//            $("#regSuccess").modal("show");
-//            $.ajax({
-//                type: "POST",
-//                url: "api/auth/login",
-//                data: ({
-//                    login: $("#email").val(),
-//                    pass: $("#password").val()
-//                }),
-//                success: function(data) {
-//                    $("#profileLink").html($("#name").val() + " " + $("#lname").val());
-//                    document.cookie = "token=" + data;
-//                    $("#loginForm").css("display", "none");
-//                    $("#logged").css("display", "block");
-//                    $("#email").val("");
-//                    $("#name").val("");
-//                    $("#lname").val("");
-//                    $("#password").val("");
-//                    $("#password2").val("");
-//                }
-//            });
-//        },
-//        error: function(data) {
-//            $("#regErr").css("display", "inline");
-//            $("#regErr").html(data.responseText);
-//            return false;
-//        }
-//    });
-});
+function addHelpDeskRequest() {
+
+}
