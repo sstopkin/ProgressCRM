18c05901f48d881dee9f0a75ea74cced99b5cf34
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2013-09-19 08:19:12 +0000

    help desk

--- ProgressCRM/src/java/org/progress/crm/controllers/HelpDeskController.java
+++ ProgressCRM/src/java/org/progress/crm/controllers/HelpDeskController.java
@@ -2,23 +2,31 @@ package org.progress.crm.controllers;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import org.hibernate.Session;
 import org.progress.crm.dao.DaoFactory;
 import org.progress.crm.exceptions.CustomException;
+import org.progress.crm.exceptions.IsNotAuthenticatedException;
 
 @Singleton
 public class HelpDeskController {
+
+    @EJB
+    AuthenticationManager authManager;
 
     public List getAllHelpDeskRequest(Session session) throws CustomException, SQLException {
         return DaoFactory.getHelpDeskDao().getAllHelpDeskRequests(session);
     }
 
     public boolean addHelpDeskRequest(Session session, String token, String request, String description) throws CustomException, SQLException {
-//        if (token == null) {
-//            throw new IsNotAuthenticatedException();
-//        }
-        DaoFactory.getHelpDeskDao().addHelpDeskRequest(session, Integer.valueOf("666"), request, description, Integer.valueOf("1"));
+        if (token == null) {
+            throw new IsNotAuthenticatedException();
+        }
+        UUID uuid = UUID.fromString(token);
+        int idWorker = authManager.getUserIdByToken(uuid);
+        DaoFactory.getHelpDeskDao().addHelpDeskRequest(session, idWorker, request, description, Integer.valueOf("1"));
         return true;
     }
 }
