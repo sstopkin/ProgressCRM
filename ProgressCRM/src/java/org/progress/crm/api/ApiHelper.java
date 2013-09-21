2199ac7ce9b8dafdf791af3b17de740192ecfd53
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2013-09-21 16:39:19 +0000

    price impl

--- ProgressCRM/src/java/org/progress/crm/api/ApiHelper.java
+++ ProgressCRM/src/java/org/progress/crm/api/ApiHelper.java
@@ -1,5 +1,6 @@
 package org.progress.crm.api;
 
+import java.io.File;
 import javax.ws.rs.core.Response;
 
 class ApiHelper {
@@ -17,5 +18,12 @@ class ApiHelper {
             return getResponse(false);
         }
         return Response.ok(text).build();
+    }
+
+    public static Response getResponse(File f) {
+        Response.ResponseBuilder response = Response.ok((Object) f);
+        response.header("Content-Disposition",
+                "attachment; filename=report.pdf");
+        return response.build();
     }
 }
