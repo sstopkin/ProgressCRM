f27802b3fe76a59955ff0c945b2e9dce847ffc3f
stopkin.sergey@gmail.com <sergeystopkin>
2013-09-23 08:05:32 +0000

    PDF

--- ProgressCRM/src/java/org/progress/crm/dao/ReportGeneratorDao.java
+++ ProgressCRM/src/java/org/progress/crm/dao/ReportGeneratorDao.java
@@ -1,11 +1,21 @@
 package org.progress.crm.dao;
 
 import java.io.File;
+import org.hibernate.Session;
+import org.progress.crm.exceptions.CustomException;
+import org.progress.crm.logic.Apartaments;
 import org.progress.crm.util.PDF;
 
 public class ReportGeneratorDao {
 
-    public File reportGen() {
+    public File priceGen(Session session) {
+
+
+        return PDF.Gen();
+    }
+
+    public File apartamentsPageGen(Session session, Integer apartamentsId) throws CustomException {
+        Apartaments apart = DaoFactory.getApartamentsDao().getApartamentsById(session, apartamentsId);
         return PDF.Gen();
     }
 }
