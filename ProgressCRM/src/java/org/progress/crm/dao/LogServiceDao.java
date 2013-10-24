c72d647112c1c52df33f6a8db36353a3cd231f89
stopkin.sergey@gmail.com <sergeystopkin>
2013-10-24 13:59:26 +0000

    logservice (1/..)

---
+++ ProgressCRM/src/java/org/progress/crm/dao/LogServiceDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.progress.crm.dao;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.progress.crm.logic.LogService;
+
+/**
+ *
+ * @author best
+ */
+public class LogServiceDao {
+//
+//    public boolean addNewAction(final Session session, int idWorker, int action) {
+//        return (boolean) session.save(new LogService(idWorker, action));
+//    }
+//
+//    public List<LogService> getFullLog(Session session) {
+//        List<LogService> list = session.createCriteria(LogService.class).list();
+//        return list;
+//    }
+}
---
+++ ProgressCRM/src/java/org/progress/crm/logic/LogService.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.progress.crm.logic;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author best
+ */
+//@Entity
+//@Table(name = "LogService")
+public class LogService implements Serializable {
+//
+//    private int id;
+//    private int idWorker;
+//    private Date date;
+//    private int action;
+//
+//    public LogService() {
+//    }
+//
+//    public LogService(int idWorker, int action) {
+//        this.idWorker = idWorker;
+//        this.date = new Date();
+//        this.action = action;
+//    }
+//
+//    @Column(name = "Action")
+//    public int getAction() {
+//        return action;
+//    }
+//
+//    public void setAction(int action) {
+//        this.action = action;
+//    }
+//
+//    @Id
+//    @Column(name = "id")
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    @Column(name = "idWorker")
+//    public int getIdWorker() {
+//        return idWorker;
+//    }
+//
+//    public void setIdWorker(int idWorker) {
+//        this.idWorker = idWorker;
+//    }
+//
+//    @Column(name = "Date")
+//    @Temporal(TemporalType.TIMESTAMP)
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
+}
