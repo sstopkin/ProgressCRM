c38411d64232dc44a8a051a4ae49239ade87ceef
stopkin.sergey@gmail.com <sergeystopkin>
2013-10-25 15:57:24 +0000

    fuck log service >_<

--- ProgressCRM/src/java/org/progress/crm/controllers/LogServiceController.java
+++ ProgressCRM/src/java/org/progress/crm/controllers/LogServiceController.java
@@ -1,8 +1,12 @@
 package org.progress.crm.controllers;
 
+import java.sql.SQLException;
 import javax.ejb.Singleton;
 import org.hibernate.Session;
 import org.progress.crm.dao.DaoFactory;
+import org.progress.crm.exceptions.CustomException;
+import org.progress.crm.util.Command;
+import org.progress.crm.util.TransactionService;
 
 /**
  *
@@ -11,8 +15,13 @@ import org.progress.crm.dao.DaoFactory;
 @Singleton
 public class LogServiceController {
 
-    public boolean addEvent(Session session, int idWorker, String description, int action) {
-        DaoFactory.getLogServiceDao().addNewAction(session, idWorker, description, action);
-        return true;
+    public boolean addEvent(final int idWorker, final String description, final int action) throws CustomException {
+        return TransactionService.runInScope(new Command<Boolean>() {
+            @Override
+            public Boolean execute(Session session) throws CustomException, SQLException {
+                DaoFactory.getLogServiceDao().addNewAction(session, idWorker, description, action);
+                return true;
+            }
+        });
     }
 }
