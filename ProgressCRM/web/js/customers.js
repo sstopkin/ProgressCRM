86797a32a5f8bec1f03893ff4a233df2f7cc590f
stopkin.sergey@gmail.com <sergeystopkin>
2013-10-31 18:35:59 +0000

    js: add customer

--- ProgressCRM/web/js/customers.js
+++ ProgressCRM/web/js/customers.js
@@ -72,10 +72,10 @@ function getCustomersPage() {
 }
 
 function addCustomer() {
-    console.log("addCustomerById");
+    console.log("addCustomer");
     $("#addCustomer").css("display", "block");
     $("#errorBlock").css("display", "none");
-    
+
     $("#customersAddReadyLink").css("display", "inline");
     $("#customersAddReadyLink").click(function() {
 //        if (
@@ -100,78 +100,21 @@ function addCustomer() {
 //            $("#errorBlock").css("display", "block");
 //            return false;
 //        }
-        var pursale;
-        $("#PureSale").click(function() {
-            if (document.getElementById("PureSale").checked) {
-                pursale = true;
-            } else {
-                pursale = false;
-            }
-        });
-        var mortgage;
-        $("#Mortgage").click(function() {
-            if (document.getElementById("Mortgage").checked) {
-                mortgage = true;
-            } else {
-                mortgage = false;
-            }
-        });
-        var exchange;
-        $("#Exchange").click(function() {
-            if (document.getElementById("Exchange").checked) {
-                exchange = true;
-            } else {
-                exchange = false;
-            }
-        });
-        var rent;
-        $("#Rent").click(function() {
-            if (document.getElementById("Rent").checked) {
-                rent = true;
-            } else {
-                rent = false;
-            }
-        });
-        var replanning;
-        $("#RePlanning").click(function() {
-            if (document.getElementById("RePlanning").checked) {
-                replanning = true;
-            } else {
-                replanning = false;
-            }
-        });
         $.ajax({
             type: "POST",
-            url: "api/apartament/addapartament",
+            url: "api/customers/addcustomer",
             data: ({
-                typeofsales: $('#TypeOfSales').val(),
-                cityName: $('#apartamentCity').text(),
-                streetName: $('#apartamentStreet').text(),
-                houseNumber: $('#apartamentBuilding').text(),
-                buildingNumber: $('#apartamentBuildingAdd').text(),
-                rooms: $('#Rooms').val(),
-                kladrId: $('#Price').val(),
-                shortAddress: $('#address').text(),
-                price: $('#Price').val(),
-                citydistrict: $('#CityDistrict').val(),
-                floor: $('#Floor').val(),
-                floors: $('#Floors').val(),
-                roomnumber: $('#RoomNumber').val(),
-                material: $('#Material').val(),
-                sizeapartament: $('#SizeApartament').val(),
-                sizeliving: $('#SizeLiving').val(),
-                sizekitchen: $('#SizeKitchen').val(),
-                balcony: $('#Balcony').val(),
-                loggia: $('#Loggia').val(),
-                yearofconstruction: $('#YearOfConstruction').val(),
-                description: $('#Description').val(),
-                puresale: pursale,
-                mortgage: mortgage,
-                exchange: exchange,
-                rent: rent,
-                replanning: replanning,
-                clientphone: $('#ClientPhone').val(),
-                clientdescription: $('#ClientDescription').val(),
+                customersFname: $('#customersFname').val(),
+                customersMname: $('#customersMname').val(),
+                customersLname: $('#customersLname').val(),
+                customersYearOfBirthday: $('#customersYearOfBirthday').val(),
+                customersMonthOfBirthday: $('#customersMonthOfBirthday').val(),
+                customersDayOfBirthday: $('#customersDayOfBirthday').val(),
+                customersSex: $('#customersSex').val(),
+                customersEmail: $('#customersEmail').val(),
+                customersPhone: $('#customersPhone').val(),
+                customersAddress: $('#customersAddress').val(),
+                customersExtra: $('#customersExtra').val()
             }),
             success: function(data) {
                 location.reload();//FIXME
