64c5d7697043a156be497fcc29b632ea9f37464c
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2013-12-07 13:54:32 +0000

    CustomersRent (4 of ..)

--- ProgressCRM/web/js/customersrent.js
+++ ProgressCRM/web/js/customersrent.js
@@ -115,47 +115,50 @@ function writeToDivCustomersRentList(data) {
     }).responseText;
     var array = JSON.parse(data);
     var str = array;
-//    var str = "<table class=\"table table-bordered\">";
-//    str += "<thead>";
-//    str += "<tr>";
-//    str += "<th>#</th>";
-//    str += "<th>Улица</th>";
-//    str += "<th>Номер дома</th>";
-//    str += "<th>Кол-во комнат</th>";
-//    str += "<th>Этаж</th>";
-//    str += "<th>Описание</th>";
-//    str += "<th>Автор</th>";
-//    str += "<th>Дата</th>";
+    var str = "<table class=\"table table-bordered\">";
+    str += "<thead>";
+    str += "<tr>";
+    str += "<th>#</th>";
+    str += "<th>Назначена</th>";
+    str += "<th>Статус</th>";
+    str += "<th>Клиент</th>";
+    str += "<th>Описание</th>";
+    str += "<th>Автор</th>";
+    str += "<th>Дата</th>";
 //    if (permissions == "3") {
 //        str += "<th>Редактировать</th>";
 //        str += "<th>Удалить</th>";
 //    }
-//    str += "</tr>";
-//    str += "</thead>";
-//    str += "<tbody>";
-//    array.forEach(function(entry) {
-//        str += "<tr>";
-//        str += "<td><a href=\"#customersrent/view/"+entry.id+"\"\">" + entry.id + "</a></td>";
-//        str += "<td>" + entry.street + "</td>";
-//        str += "<td>" + entry.houseNumber + "</td>";
-//        str += "<td>" + entry.rooms + "</td>";
-//        str += "<td>" + entry.floor + " / " + entry.floors + "</td>";
-//        str += "<td>" + entry.description + "</td>";
-//        for (var i = 0; i < workersList.length; ++i) {
-//            var a = workersList[i];
-//            if (entry.idWorker == a[0]) {
-//                str += "<td>" + a[1] + " " + a[3] + "</td>";
-//            }
-//        }
-//        str += "<td>" + entry.creationDate + "</td>";
-//
-//        if (permissions == "3") {
-//            str += "<td>" + "<button type=\"button\" onclick=\"editCustomersRentById(" + entry.id + ");\" class=\"btn btn-default\"><span class=\"glyphicon glyphicon-pencil\"></span></button>" + "</td>";
-//            str += "<td>" + "<button type=\"button\" onclick=\"deleteCustomersRentById(" + entry.id + ");\" class=\"btn btn-default\"><span class=\"glyphicon glyphicon-remove\"></span></button>" + "</td>";
-//        }
-//        str += "</tr>";
-//    });
-//    str += "</tbody>";
+    str += "</tr>";
+    str += "</thead>";
+    str += "<tbody>";
+    array.forEach(function(entry) {
+        str += "<tr>";
+        str += "<td><a href=\"#customersrent/view/" + entry.id + "\"\">" + entry.id + "</a></td>";
+        for (var i = 0; i < workersList.length; ++i) {
+            var a = workersList[i];
+            if (entry.assigned == a[0]) {
+                str += "<td>" + a[1] + " " + a[3] + "</td>";
+            }
+        }
+        str += "<td>" + taskStatusList[entry.status][1]+ "</td>";
+        str += "<td>" + entry.idCustomer + "</td>";
+        str += "<td>" + entry.description + "</td>";
+        for (var i = 0; i < workersList.length; ++i) {
+            var a = workersList[i];
+            if (entry.idWorker == a[0]) {
+                str += "<td>" + a[1] + " " + a[3] + "</td>";
+            }
+        }
+        
+        str += "<td>" + entry.creationDate + "</td>";
+        if (permissions == "3") {
+            str += "<td>" + "<button type=\"button\" onclick=\"editCustomersRentById(" + entry.id + ");\" class=\"btn btn-default\"><span class=\"glyphicon glyphicon-pencil\"></span></button>" + "</td>";
+            str += "<td>" + "<button type=\"button\" onclick=\"deleteCustomersRentById(" + entry.id + ");\" class=\"btn btn-default\"><span class=\"glyphicon glyphicon-remove\"></span></button>" + "</td>";
+        }
+        str += "</tr>";
+    });
+    str += "</tbody>";
     $("#mainCustomersRentContainer").html(str);
 }
 
@@ -168,6 +171,6 @@ function customersRentShowAddCustomerModal() {
         $("#customersRentAssigned").append('<option value="' + entry[0] + '">' + entry[1] + " " + entry[2] + " " + entry[3] + '</option>');
     });
     taskStatusList.forEach(function(entry) {
-        $("#customersRentStatus").append('<option value="' + entry[0] + '">' + entry[1]+'</option>');
+        $("#customersRentStatus").append('<option value="' + entry[0] + '">' + entry[1] + '</option>');
     });
 }
