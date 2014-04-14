1a154e9bbfe2e364a9a04cf17d14dbf0bba4dd54
stopkin.sergey@gmail.com <sergeystopkin>
2014-04-14 19:26:09 +0000

    redesign apartamentsadd remove more duplicate code search block fix

--- ProgressCRM/src/main/webapp/js/search.js
+++ ProgressCRM/src/main/webapp/js/search.js
@@ -1,31 +1,37 @@
-function initSearchForm() {
-    $.get("search.html", function(data) {
-        $("#mainSearchContainer").html(data);
-        var date = new Date();
-        var day = date.getDate();
-        day = (parseInt(day, 10) < 10) ? ('0' + day) : (day);
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
+function initSearchForm(type) {
+    if (type === "undefined") {
+        $("#mainSearchContainer").html("");
+    }
+    else {
+        $.get("search.html", function(data) {
+            $("#mainSearchContainer").html(data);
+            var date = new Date();
+            var day = date.getDate();
+            day = (parseInt(day, 10) < 10) ? ('0' + day) : (day);
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear();
 //        $('#announcementsSearchStartDate').val(year + "-" + month + "-" + day);
-        $('#mainSearchStartDate').datepicker({
-            format: 'yyyy-mm-dd'
-        });
+            $('#mainSearchStartDate').datepicker({
+                format: 'yyyy-mm-dd'
+            });
 //        $('#announcementsSearchEndDate').val(year + "-" + month + "-" + day);
-        $('#mainSearchEndDate').datepicker({
-            format: 'yyyy-mm-dd'
+            $('#mainSearchEndDate').datepicker({
+                format: 'yyyy-mm-dd'
+            });
+            $("#mainSearchAuthor").append('<option value="">Все</option>');
+            workersList.forEach(function(entry) {
+                $("#mainSearchAuthor").append('<option value="' + entry[0] + '">' + entry[1] + " " + entry[2] + " " + entry[3] + '</option>');
+            });
+            $("#mainSearchAssigned").append('<option value="">Все</option>');
+            workersList.forEach(function(entry) {
+                $("#mainSearchAssigned").append('<option value="' + entry[0] + '">' + entry[1] + " " + entry[2] + " " + entry[3] + '</option>');
+            });
+            $("#mainSearchActionButtonDiv").html("<input onclick=\"mainSearchAction('" + type + "');\" type=\"button\" class=\"btn btn-primary pull-right\" value=\"Поиск\">");
         });
-        $("#mainSearchAuthor").append('<option value="">Все</option>');
-        workersList.forEach(function(entry) {
-            $("#mainSearchAuthor").append('<option value="' + entry[0] + '">' + entry[1] + " " + entry[2] + " " + entry[3] + '</option>');
-        });
-        $("#mainSearchAssigned").append('<option value="">Все</option>');
-        workersList.forEach(function(entry) {
-            $("#mainSearchAssigned").append('<option value="' + entry[0] + '">' + entry[1] + " " + entry[2] + " " + entry[3] + '</option>');
-        });
-    });
+    }
 }
 
-function mainSearchAction() {
+function mainSearchAction(type) {
     var permissions = $.ajax({
         type: "GET",
         url: "api/auth/validate",
@@ -45,10 +51,10 @@ function mainSearchAction() {
                 "&startdate=" + $("#mainSearchStartDate").val() +
                 "&enddate=" + $("#mainSearchEndDate").val() +
                 "&contains=" + $("#mainSearchContains").val() +
-                "&type=apartaments",
+                "&type=" + type,
         success: function(data) {
             $("#errorBlock").css("display", "none");
-            drawTable(permissions,data);
+            drawTable(permissions, data);
         },
         error: function(data) {
             showDanger(data.responseText);
