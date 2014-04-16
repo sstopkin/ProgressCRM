b71e2f0e61133b84d6fd3650e63bfdde8195273e
stopkin.sergey@gmail.com <sergeystopkin>
2014-04-16 20:04:12 +0000

    validation fix

--- ProgressCRM/src/main/java/org/progress/crm/controllers/ApartamentsController.java
+++ ProgressCRM/src/main/java/org/progress/crm/controllers/ApartamentsController.java
@@ -15,10 +15,10 @@ import org.progress.crm.logic.Apartaments;
 
 @Singleton
 public class ApartamentsController {
-
+    
     @EJB
     AuthenticationManager authManager;
-
+    
     public Apartaments getApartamentById(Session session, String token, String apartamentId) throws CustomException {
         if (apartamentId == null) {
             throw new BadRequestException();
@@ -28,7 +28,7 @@ public class ApartamentsController {
         }
         return DaoFactory.getApartamentsDao().getApartamentsById(session, Integer.valueOf(apartamentId));
     }
-
+    
     public boolean addApartament(Session session, String token, String typeOfSales,
             String cityName, String streetName, String houseNumber, String buildingNumber, String kladrId,
             String shortAddress, String apartamentLan, String apartamentLon, String rooms, String dwellingType,
@@ -42,7 +42,7 @@ public class ApartamentsController {
         }
         UUID uuid = UUID.fromString(token);
         int idWorker = authManager.getUserIdByToken(uuid);
-
+        
         int typeOfSalesInt = typeOfSales.equals("") ? 0 : Integer.valueOf(typeOfSales);
         int roomsInt = rooms.equals("") ? 0 : Integer.valueOf(rooms);
         int dwellingTypeInt = dwellingType.equals("-1") ? 0 : Integer.valueOf(dwellingType);
@@ -54,11 +54,12 @@ public class ApartamentsController {
         int materialInt = material.equals("-1") ? 0 : Integer.valueOf(material);
         int balconyInt = balcony.equals("-1") ? 0 : Integer.valueOf(balcony);
         int loggiaInt = loggia.equals("-1") ? 0 : Integer.valueOf(loggia);
+        int statusInt = status.equals("") ? 0 : Integer.valueOf(status);
         int yearOfConstructionInt = yearOfConstruction.equals("") ? 0 : Integer.valueOf(yearOfConstruction);
         BigDecimal sizeApartamentBig = sizeApartament.equals("") ? BigDecimal.ZERO : new BigDecimal(sizeApartament);
         BigDecimal sizeLivingBig = sizeLiving.equals("") ? BigDecimal.ZERO : new BigDecimal(sizeLiving);
         BigDecimal sizeKitchenBig = sizeKitchen.equals("") ? BigDecimal.ZERO : new BigDecimal(sizeKitchen);
-
+        
         DaoFactory.getApartamentsDao().addApartament(session, typeOfSalesInt,
                 cityName, streetName, houseNumber, buildingNumber, kladrId, shortAddress,
                 apartamentLan, apartamentLon, roomsInt, dwellingTypeInt, priceInt, cityDistrictInt, floorInt,
@@ -66,10 +67,10 @@ public class ApartamentsController {
                 loggiaInt, yearOfConstructionInt, description,
                 Boolean.parseBoolean(pureSale), Boolean.parseBoolean(mortgage),
                 Boolean.parseBoolean(exchange), Boolean.parseBoolean(rent),
-                Boolean.parseBoolean(rePplanning), idWorker, Integer.valueOf(idWorkerTarget), Integer.valueOf(idCustomer), Integer.valueOf(status), false);
+                Boolean.parseBoolean(rePplanning), idWorker, Integer.valueOf(idWorkerTarget), Integer.valueOf(idCustomer), statusInt, false);
         return true;
     }
-
+    
     public boolean editApartament(Session session, String token, String apartamentsId,
             String typeOfSales, String rooms, String dwellingType, String price, String cityDistrict, String floor, String floors, String roomNumber,
             String material, String sizeApartament, String sizeLiving, String sizeKitchen,
@@ -83,7 +84,7 @@ public class ApartamentsController {
         }
         UUID uuid = UUID.fromString(token);
         int idWorker = authManager.getUserIdByToken(uuid);
-
+        
         Apartaments apartaments = DaoFactory.getApartamentsDao().getApartamentsById(session, Integer.valueOf(apartamentsId));
         apartaments.setBalcony(Integer.valueOf(balcony));
         apartaments.setCityDistrict(Integer.valueOf(cityDistrict));
@@ -114,7 +115,7 @@ public class ApartamentsController {
         DaoFactory.getApartamentsDao().modifyApartament(session, apartaments);
         return true;
     }
-
+    
     public boolean removeApartament(Session session, String token, String apartamentsId) throws CustomException {
         if (apartamentsId == null) {
             throw new BadRequestException();
@@ -124,13 +125,13 @@ public class ApartamentsController {
         }
         UUID uuid = UUID.fromString(token);
         int idWorker = authManager.getUserIdByToken(uuid);
-
+        
         Apartaments apartaments = DaoFactory.getApartamentsDao().getApartamentsById(session, Integer.valueOf(apartamentsId));
         apartaments.setDeleted(true);
         DaoFactory.getApartamentsDao().modifyApartament(session, apartaments);
         return true;
     }
-
+    
     public List<Apartaments> getAllApartament(Session session, String token, boolean prepare) throws CustomException {
         if (token == null) {
             throw new IsNotAuthenticatedException();
