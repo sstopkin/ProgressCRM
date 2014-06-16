44d5e88f39e45bdc3732a82624ff779b554ad874
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2014-06-16 19:57:49 +0000

    impl new func

---
+++ ProgressCRM/src/main/java/org/progress/crm/controllers/StatsController.java
@@ -0,0 +1,13 @@
+package org.progress.crm.controllers;
+
+import javax.ejb.Singleton;
+import org.hibernate.Session;
+
+@Singleton
+public class StatsController {
+
+    public Object getCounts(Session session, String token) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
