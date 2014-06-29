633dea49dee7fa936a04d81864431c934dd254c5
stopkin.sergey@gmail.com <sergeystopkin>
2014-06-29 19:40:25 +0000

    rewrite admin

--- ProgressCRM/src/main/webapp/js/admin.js
+++ ProgressCRM/src/main/webapp/js/admin.js
@@ -1,56 +1,54 @@
-//var taskId;
-//var access;
-//var codes = [];
-
-//function getUnevaluatedTasks() {
-//    $.ajax({
-//        type: "GET",
-//        url: "api/admin/getunevaluated",
-//        success: function(data) {
-//            var list = JSON.parse(data);
-//            var str = "";
-//            str += "<table class=\"table table-hover table-bordered\" style='margin-top:10px; margin-bottom:10px;' >";
-//            str += "<thead class='t-header'><tr>";
-//            str += "<th class=\"col-md-3\">Email</th>";
-//            str += "<th class=\"col-md-4\">Задание</th>";
-//            str += "<th class=\"col-md-2\">Исходный код</th>";
-//            str += "<th class=\"col-md-3\">Оценка</th>";
-//            str += "</tr></thead>";
-//            str += "<tbody class='t-cell'>";
-//
-//            ids_tasks = [];
-//            for (var j = 0; j < list.length; ++j) {
-//                ids_tasks[j] = list[j][0].taskId;
-//                str += "<tr><td>";
-//                str += list[j][1];
-//                str += "</td><td>";
-//                str += "<a href=\"#task/view/" + list[j][0].taskId + "\">" + list[j][2] + "</a>";
-//                str += "</td><td>";
-//                codes[j] = list[j][0].source;
-//                str += "<a href=\"\" onclick=\"return viewCode(" + j + ");\">Просмотр кода</a>";
-//                str += "</td><td align='center'>";
-//                str += "<div class='btn-group'>";
-//                str += "<button type='button' onclick=\"return checkSuccessTask(" + list[j][0].id + ");\" class='btn btn-success'>Верно</button>";
-//                str += "<button type='button' onclick=\"return checkFailTask(" + list[j][0].id + ");\" class='btn btn-danger'>Неверно</button>";
-//                str += "</div>";
-//                str += "</td></tr>";
-//            }
-//            str += "</tbody></table>";
-//            $("#adminContent").html(str);
-//        },
-//        error: function(data) {
-//            $("#errorBlock").addClass("alert-danger");
-//            $("#errorMessage").html(data.responseText);
-//            $("#errorBlock").css("display", "block");
-//            checkStatus();
-//            return false;
-//        }
-//    });
-//    return false;
-//}
-
 function getUsersList() {
     $.ajax({
+        type: "GET",
+        url: "api/workers/getallworkers",
+        success: function(data) {
+            var list = JSON.parse(data);
+            var str = '<table class="table table-striped table-bordered" cellspacing="0" width="100%" id="usersListTable">';
+            str += "<thead class='t-header'><tr>";
+            str += "<th class=\"col-md-1\">id</th>";
+            str += "<th class=\"col-md-1\">Email</th>";
+            str += "<th class=\"col-md-1\">Фамилия</th>";
+            str += "<th class=\"col-md-1\">Имя</th>";
+            str += "<th class=\"col-md-1\">Отчество</th>";
+            str += "<th class=\"col-md-1\">Статус</th>";
+            str += "</tr></thead>";
+            str += "<tbody class='t-cell'>";
+
+            for (var j = 0; j < list.length; ++j) {
+                str += "<tr><td>";
+                str += "<a href=\"#workers/view/" + list[j][0] + "\" class=\"btn btn-primary\"><b>" + list[j][0] + "</b></a>";
+                str += "</td><td>";
+                str += list[j][1];
+                str += "</td><td>";
+                str += list[j][2];
+                str += "</td><td>";
+                str += list[j][3];
+                str += "</td><td>";
+                str += list[j][4];
+                str += "</td><td>";
+                if (list[j][6] == true) {
+                    str += "<a href=\"\" onclick=\"return banUser(" + list[j][0] + ");\"><span class=\"label label-success\">Активен</span></a>";
+                } else {
+                    str += "<a href=\"\" onclick=\"return unBanUser(" + list[j][0] + ");\"><span class=\"label label-danger\">Заблокирован</span></a>";
+                }
+                str += "</td></tr>";
+            }
+            str += "</tbody></table>";
+            $("#adminContent").html(str);
+            $('#usersListTable').dataTable();
+        },
+        error: function(data) {
+            showDanger(data.responseText);
+            checkStatus();
+            return false;
+        }
+    });
+    return false;
+}
+
+function getCallsStatsPage() {
+$.ajax({
         type: "GET",
         url: "api/workers/getallworkers",
         success: function(data) {
