ae507bac5b11891c17e7f04ef68fd720c9e6c3e5
stopkin.sergey@gmail.com <sergeystopkin>
2014-07-01 08:44:44 +0000

    planner impl

--- ProgressCRM/src/main/java/org/progress/crm/api/PlannerApi.java
+++ ProgressCRM/src/main/java/org/progress/crm/api/PlannerApi.java
@@ -12,6 +12,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.hibernate.Session;
 import org.progress.crm.controllers.PlannerController;
@@ -29,14 +30,50 @@ public class PlannerApi {
 
     @GET
     @Path("all")
-    public Response getAllPlannerTasks(@CookieParam("token") final String token) throws SQLException, CustomException {
+    public Response getAllPlannerTasks(@CookieParam("token") final String token,
+            @QueryParam("from") final String from,
+            @QueryParam("to") final String to,
+            @QueryParam("browser_timezone") final String timezone) throws SQLException, CustomException {
         return TransactionService.runInScope(new Command<Response>() {
             @Override
             public Response execute(Session session) throws SQLException {
                 try {
                     Gson tasksList = new GsonBuilder().create();
                     String newsJson = tasksList.toJson(plannerController.getTasks(session, token));
-                    return ApiHelper.getResponse(newsJson);
+//                    return ApiHelper.getResponse(newsJson);
+//from=1362070800000&to=1364749200000&browser_timezone=Asia%2FOmsk
+                    String responce = "{\n" + "	\"success\": 1,\n" + "	\"result\": [\n" + "		{\n"
+                            + "			\"id\": \"293\",\n"
+                            + "			\"title\": \"This is warning class event with very long title to check how it fits to evet in day view\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-warning\",\n"
+                            + "			\"start\": \"1362938400000\",\n" + "			\"end\":   \"1363197686300\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"256\",\n" + "			\"title\": \"Event that ends on timeline\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-warning\",\n"
+                            + "			\"start\": \"1363155300000\",\n" + "			\"end\":   \"1363227600000\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"276\",\n" + "			\"title\": \"Short day event\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-success\",\n"
+                            + "			\"start\": \"1363245600000\",\n" + "			\"end\":   \"1363252200000\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"294\",\n" + "			\"title\": \"This is information class \",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-info\",\n"
+                            + "			\"start\": \"1363111200000\",\n" + "			\"end\":   \"1363284086400\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"297\",\n" + "			\"title\": \"This is success event\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-success\",\n"
+                            + "			\"start\": \"1363234500000\",\n" + "			\"end\":   \"1363284062400\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"54\",\n" + "			\"title\": \"This is simple event\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"\",\n"
+                            + "			\"start\": \"1363712400000\",\n" + "			\"end\":   \"1363716086400\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"532\",\n" + "			\"title\": \"This is inverse event\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-inverse\",\n"
+                            + "			\"start\": \"1364407200000\",\n" + "			\"end\":   \"1364493686400\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"548\",\n" + "			\"title\": \"This is special event\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-special\",\n"
+                            + "			\"start\": \"1363197600000\",\n" + "			\"end\":   \"1363629686400\"\n" + "		},\n"
+                            + "		{\n" + "			\"id\": \"295\",\n" + "			\"title\": \"Event 3\",\n"
+                            + "			\"url\": \"http://www.example.com/\",\n" + "			\"class\": \"event-important\",\n"
+                            + "			\"start\": \"1364320800000\",\n" + "			\"end\":   \"1364407286400\"\n" + "		}\n"
+                            + "	]\n" + "}";
+
+                    return ApiHelper.getResponse(responce);
                 } catch (CustomException ex) {
                     Logger.getLogger(PlannerApi.class.getName()).log(Level.SEVERE, null, ex);
                     return ApiHelper.getResponse(ex);
