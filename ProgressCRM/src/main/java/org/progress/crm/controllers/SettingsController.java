3cce84f35923119041494d1824d641580dc56660
stopkin.sergey@gmail.com <stopkin.sergey@gmail.com@68d3575d-054e-f20c-82cb-2ea25c6451c1>
2014-12-14 20:16:33 +0000

    settings impl

--- ProgressCRM/src/main/java/org/progress/crm/controllers/SettingsController.java
+++ ProgressCRM/src/main/java/org/progress/crm/controllers/SettingsController.java
@@ -1,13 +1,21 @@
 package org.progress.crm.controllers;
 
+import com.google.common.collect.HashBiMap;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import org.hibernate.Session;
 import org.progress.crm.dao.DaoFactory;
 import org.progress.crm.exceptions.IsNotAuthenticatedException;
+import org.progress.crm.scheduled.CronJob;
+import org.progress.crm.util.HibernateUtil;
+import java.util.logging.Logger;
+import org.progress.crm.logic.Settings;
 
 @Singleton
 public class SettingsController {
@@ -15,11 +23,27 @@ public class SettingsController {
     @EJB
     AuthenticationManager authenticationManager;
 
-    public List getParameters(Session session, String token) throws IsNotAuthenticatedException, SQLException {
-        if (token == null) {
-            throw new IsNotAuthenticatedException();
+    private static final Logger log = Logger.getLogger(SettingsController.class.getName());
+    public static Map<String, String> parameters = null;
+
+    static {
+        Session session = null;
+        parameters=new HashMap<>();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            try {
+                List<Settings> list = DaoFactory.getSettingsDao().getSettingsByWorkerId(session, 1);
+                for (Settings item : list) {
+                    parameters.put(item.getParameter(), item.getValue());
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CronJob.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
         }
-        return DaoFactory.getSettingsDao().getSettingsByWorkerId(session, 1);
     }
 
     public List getParametersByWorkerId(Session session, String token) throws IsNotAuthenticatedException, SQLException {
--- ProgressCRM/src/main/java/org/progress/crm/scheduled/CronJob.java
+++ ProgressCRM/src/main/java/org/progress/crm/scheduled/CronJob.java
@@ -1,19 +1,10 @@
 package org.progress.crm.scheduled;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Date;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import org.progress.crm.controllers.CustomersController;
 
-/**
- *
- * @author best
- */
 @Singleton
 public class CronJob {
 
@@ -31,36 +22,38 @@ public class CronJob {
 //                "running every minute .. now it's: " + new Date().toString());
 //    }
 //
-    @Schedule(minute = "*/5", hour = "*")
-    public void runEvery10Second() {
-        String filename = "/tmp/progresscrm.log";
-        try {
-            try (FileWriter sw = new FileWriter(filename, true)) {
-                sw.write("log " + new Date().toString() + "\n");
-            }
-        } catch (IOException e) {
-            log.log(Level.INFO, e.getMessage());
-        }
-    }
+//    @Schedule(minute = "*/5", hour = "*")
+//    public void runEvery10Second() {
+//        String filename = "/tmp/progresscrm.log";
+//        try {
+//            try (FileWriter sw = new FileWriter(filename, true)) {
+//                sw.write("log " + new Date().toString() + "\n");
+//            }
+//        } catch (IOException e) {
+//            log.log(Level.INFO, e.getMessage());
+//        }
+//    }
 //    @Schedule(second = "*/5", minute = "*", hour = "*")
 //    @Schedule(dayOfMonth = "*")
-//    public void runEveryDay() throws CustomException {
+//    @Schedule(minute = "*/5", hour = "*")
+//    public void runEveryDay() throws CustomException, SQLException {
 //        Session session = null;
-//        try {
-//            session = HibernateUtil.getSessionFactory().openSession();
-//            session.beginTransaction();
-//            List<Customers> list = customersController.getCustomersListByBirthday(session, new Date());
-//            for (Customers customers : list) {
-//                log.log(Level.INFO,
-//                        ".. now it's: " + new Date().toString() + " " + customers.getCustomersFname());
-//            }
-//            session.getTransaction().commit();
-////        } catch (HibernateException | SQLException | NoSuchAlgorithmException | IOException | InterruptedException | ExecutionException e) {
-////            session.getTransaction().rollback();
-//        } finally {
-//            if (session != null && session.isOpen()) {
-//                session.close();
-//            }
+//
+//        session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//        List<Customers> list = customersController.getCustomersListByBirthday(session, new Date());
+//        for (Customers customers : list) {
+//            log.log(Level.INFO,
+//                    ".. now it's: " + new Date().toString() + " " + customers.getCustomersFname());
 //        }
+//        String filename = "/tmp/progresscrm.log";
+//        try {
+//            try (FileWriter sw = new FileWriter(filename, true)) {
+//                sw.write("log " + new Date().toString() + "\n");
+//            }
+//        } catch (IOException e) {
+//            log.log(Level.INFO, e.getMessage());
+//        }
+//
 //    }
 }
