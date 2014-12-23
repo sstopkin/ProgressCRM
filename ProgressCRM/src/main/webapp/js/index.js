681f8a108ea1b0bd4c8ed9a1bbe523701519b7de
stopkin.sergey@gmail.com <sergeystopkin>
2014-12-23 18:18:00 +0000

    add sidebar clock

--- ProgressCRM/src/main/webapp/js/index.js
+++ ProgressCRM/src/main/webapp/js/index.js
@@ -59,6 +59,46 @@ function trueAuth() {
             showDanger("Ошибка загрузки настроек с сервера");
         }
     });
+    (function () {
+        var monthNames = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+        var dayNames = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+
+        // Create a newDate() object
+        var newDate = new Date();
+
+        // Extract the current date from Date object
+        newDate.setDate(newDate.getDate());
+
+        // Output the day, date, month and year   
+        $('#date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+//        setInterval(function () {
+//
+//            // Create a newDate() object and extract the seconds of the current time on the visitor's
+//            var seconds = new Date().getSeconds();
+//
+//            // Add a leading zero to seconds value
+//            $("#sec").html((seconds < 10 ? "0" : "") + seconds);
+//        }, 1000);
+
+        setInterval(function () {
+
+            // Create a newDate() object and extract the minutes of the current time on the visitor's
+            var minutes = new Date().getMinutes();
+
+            // Add a leading zero to the minutes value
+            $("#min").html((minutes < 10 ? "0" : "") + minutes);
+        }, 1000);
+
+        setInterval(function () {
+
+            // Create a newDate() object and extract the hours of the current time on the visitor's
+            var hours = new Date().getHours();
+
+            // Add a leading zero to the hours value
+            $("#hours").html((hours < 10 ? "0" : "") + hours);
+        }, 1000);
+    })();
 }
 
 function getParameterValue(array, paramName) {
